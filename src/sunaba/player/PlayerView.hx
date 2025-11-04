4b7d4842526b438e52b739d1fdd8f0169f4d83c9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-03 15:52:20 -0900

    add menu bar and game view layout to PlayerView

--- src/sunaba/player/PlayerView.hx
+++ src/sunaba/player/PlayerView.hx
@@ -2,6 +2,12 @@ package sunaba.player;
 
 import sunaba.ui.Widget;
 import sunaba.ui.Panel;
+import sunaba.ui.Control;
+import sunaba.ui.VBoxContainer;
+import sunaba.ui.MenuBar;
+import sunaba.desktop.PopupMenu;
+import sunaba.ui.SubViewportContainer;
+import sunaba.SubViewport;
 
 class PlayerView extends Widget {
     public override function init() {
