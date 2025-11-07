f7f0756340ab36d102461a9ffc677a45c0eb432b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-06 17:31:42 -0900

    Update player.gd

--- Player/player.gd
+++ Player/player.gd
@@ -15,10 +15,9 @@ func _ready() -> void:
 	PlatformService.PlatformName = "Sunaba Player"
 	var window := get_window()
 	var displayScale := DisplayServer.screen_get_scale(window.current_screen)
-	if (OS.get_name() != "Linux"):
-		if OS.get_name() == "Windows":
-			var dpi = DisplayServer.screen_get_dpi(window.current_screen)
-			displayScale = dpi * 0.01
-		window.content_scale_factor = displayScale
+	if OS.get_name() != "macOS":
+		var dpi = DisplayServer.screen_get_dpi(window.current_screen)
+		displayScale = dpi * 0.01
+	window.content_scale_factor = displayScale
 	window.size = Vector2i(1152, 648) * displayScale
 	window.move_to_center()
