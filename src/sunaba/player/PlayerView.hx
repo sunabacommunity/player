b48895e634d57c71826d2e009d71f6e0c6724da3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-11 13:04:36 -0900

    Added Custom Titlebar

--- src/sunaba/player/PlayerView.hx
+++ src/sunaba/player/PlayerView.hx
@@ -1,5 +1,12 @@
 package sunaba.player;
 
+import sunaba.core.Reference;
+import sunaba.ui.StyleBoxEmpty;
+import sunaba.core.StringArray;
+import sunaba.input.InputEventMouseButton;
+import sunaba.core.native.NativeReference;
+import sunaba.desktop.Window;
+import sunaba.core.Vector2i;
 import sunaba.core.Callable;
 import sunaba.ui.Widget;
 import sunaba.ui.Panel;
@@ -19,6 +26,11 @@ import sunaba.input.InputEvent;
 import sunaba.input.InputService;
 import haxe.Exception;
 import sunaba.desktop.FileDialog;
+import sunaba.ui.HBoxContainer;
+import sunaba.ui.Label;
+import sunaba.ui.Button;
+import sunaba.ui.CenterContainer;
+import sunaba.ui.TextureRect;
 
 class PlayerView extends Widget {
     var subViewport: SubViewport;
@@ -32,18 +44,126 @@ class PlayerView extends Widget {
         load("player://PlayerView.suml");
     }
 
+    public var window:Window;
+    public var windowSize:Vector2i;
+    private var ogWindowSize: Vector2i;
+    public var titlebarLmbPressed:Bool = false;
+    public var clickcount = 0;
+    public var timeSinceClick = 0.1;
+    public var windowTitle:Label;
+    var maximizeButton: Button;
+
+    private var resizePreview: Bool = true;
+    private var resizeThreshold: Float = 10.0;
+    private var resizeThresholdBottomRight: Float = 0.25;
+
+    var isMaximized: Bool;
+
     public override function onReady() {
+        window = getWindow();
+        var displayScale = DisplayService.screenGetScale(window.currentScreen);
+        if (OSService.getName() == "Windows") {
+            var dpi = DisplayService.screenGetDpi(window.currentScreen);
+            displayScale = dpi * 0.01;
+        }
+        window.contentScaleFactor = displayScale;
+        windowSize = new Vector2i(cast 1152 * displayScale, cast 648 * displayScale);
+        ogWindowSize = windowSize;
+        window.size = windowSize;
+        window.minSize = windowSize;
+        window.alwaysOnTop = false;
+        window.moveToCenter();
+        window.extendToTitle = true;
+        window.mode = WindowMode.windowed;
+        window.unresizable = false;
+        if (OSService.getName() == "macOS") {
+            DisplayService.windowSetWindowButtonsOffset(new Vector2i(35, 37), window.getWindowId());
+        }
+        else {
+            window.borderless = true;
+        }
+
         subViewport = getNodeT(SubViewport, "vbox/gameView/container/subViewport");
 
         abouDialog = getNodeT(AcceptDialog, "aboutDialog");
         buildAboutDialog(abouDialog);
 
         menuBarControl = getNodeT(Control, "vbox/menuBarControl");
-        if (PlatformService.osName == "macOS") {
-            menuBarControl.customMinimumSize = new Vector2(0, 0);
-        }
+        var menuBarSpacer = getNodeT(Control, "vbox/menuBarControl/hbox/spacer");
+        var eventFunc = function(eventN: NativeReference) {
+            if (window == null)
+                return;
 
-        var fileMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/File");
+            if (InputService.isMouseButtonPressed(MouseButton.left) && !titlebarLmbPressed && window.mode == WindowMode.windowed && clickcount == 0) {
+                titlebarLmbPressed = true;
+                if (eventN.isClass("InputEventMouseButton")) {
+                    var eventMouseButton = new InputEventMouseButton(eventN);
+                    clickcount++;
+                    // Top left
+                    if (eventMouseButton.position.x < resizeThreshold && eventMouseButton.position.y < resizeThreshold) {
+                        DisplayService.cursorSetShape(CursorShape.fdiagsize);
+                        window.startResize(WindowResizeEdge.topLeft);
+                        return;
+                    }
+                    // Top Right
+                    if (
+                        eventMouseButton.position.x > window.getVisibleRect().size.x - resizeThreshold &&
+                        eventMouseButton.position.y < resizeThreshold
+                    ) {
+                        DisplayService.cursorSetShape(CursorShape.bdiagsize);
+                        window.startResize(WindowResizeEdge.topRight);
+                        return;
+                    }
+                    // Top
+                    if (eventMouseButton.position.y < resizeThreshold) {
+                        DisplayService.cursorSetShape(CursorShape.vsize);
+                        window.startResize(WindowResizeEdge.top);
+                        return;
+                    }
+                    window.startDrag();
+                }        
+            }
+            else if (InputService.isMouseButtonPressed(MouseButton.left) && !titlebarLmbPressed) {
+                titlebarLmbPressed = true;
+                clickcount++;
+            }
+            else if (!InputService.isMouseButtonPressed(MouseButton.left) && titlebarLmbPressed) {
+                titlebarLmbPressed = false;
+            }
+
+            if (clickcount == 2) {
+                trace(clickcount);
+                clickcount = 0;
+                var maximizeButton = getNodeT(Button, "vbox/menuBarControl/hbox/maximizeButton");
+                if (window.mode != WindowMode.windowed) {
+                    var maximizedSize = window.size;
+                    window.mode = WindowMode.windowed;
+                    if (window.size.x == maximizedSize.x && window.size.y == maximizedSize.y) {
+                        window.size = ogWindowSize;
+                        window.moveToCenter();
+                    }
+                    else {
+                        window.size = windowSize;
+                    }
+                    maximizeButton.text = "🗖";
+                }
+                else if (window.mode == WindowMode.windowed) {
+                    windowSize = window.size;
+                    window.mode = WindowMode.maximized;
+                    maximizeButton.text = "🗗";
+                }
+            }
+        };
+
+        var menuBar: Control = getNodeT(Control, "vbox/menuBarControl/hbox/menuBar");
+        var iconContainer = getNodeT(Control, "vbox/menuBarControl/hbox/iconContainer");
+        var iconRect = getNodeT(Control, "vbox/menuBarControl/hbox/iconContainer/icon");
+        menuBar.guiInput.connect(eventFunc);
+        menuBarSpacer.guiInput.connect(eventFunc);
+        iconContainer.guiInput.connect(eventFunc);
+        iconRect.guiInput.connect(eventFunc);
+
+        var fileMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/hbox/menuBar/File");
         fileMenu.idPressed.connect(Callable.fromFunction((function(id: Int) {
             if (id == 0) {
                 var fileDialog = new FileDialog();
@@ -67,7 +187,7 @@ class PlayerView extends Widget {
             }
         })));
 
-        var viewMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/View");
+        var viewMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/hbox/menuBar/View");
         if (OSService.getName() == "macOS") {
             viewMenu.removeItem(1);
             viewMenu.setItemText(0, "Toggle Fullscreen (Cmd+F)");
@@ -81,7 +201,7 @@ class PlayerView extends Widget {
             }
         }));
 
-        var helpMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/Help");
+        var helpMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/hbox/menuBar/Help");
         if ((PlatformService.deviceType == PlatformDeviceType.desktop) && (OSService.getName() != "Windows")) {
             helpMenu.systemMenuId = 4;
         }
@@ -97,7 +217,96 @@ class PlayerView extends Widget {
             }
         }));
 
-        var window = getParent().getWindow();
+        var styleBoxEmpty = new StyleBoxEmpty();
+            
+        var buttonFont = new SystemFont();
+        if (OSService.getName() == "Windows") {
+            buttonFont.fontNames = StringArray.fromArray([
+                "Segoe MDL2 Assets",
+                "Segoe UI Symbol",
+                "Arial Unicode MS"
+            ]);
+        }
+        else if (OSService.getName() == "Linux") {
+            var fontNames = buttonFont.fontNames;
+            fontNames.add("Noto Sans Symbols2");
+            fontNames.add("DejaVu Sans");
+            buttonFont.fontNames = fontNames;
+            trace(fontNames.toArray().toString());
+            trace(buttonFont.fontNames.toArray().toString());
+        }
+
+        var minimizeButton = getNodeT(Button, "vbox/menuBarControl/hbox/minimizeButton");
+        minimizeButton.addThemeStyleboxOverride("normal", styleBoxEmpty);
+        minimizeButton.focusMode = FocusModeEnum.none;
+        minimizeButton.addThemeFontOverride("font", buttonFont);
+        minimizeButton.text = "🗕";
+        minimizeButton.alignment = HorizontalAlignment.center;
+        isMaximized = true;
+        minimizeButton.pressed.add(() -> {
+            if (window.mode != WindowMode.minimized) {
+                isMaximized = window.mode == WindowMode.maximized;
+                window.mode = WindowMode.minimized;
+            }
+            else {
+                if (isMaximized == true) {
+                    window.mode = WindowMode.maximized;
+                }
+                else {
+                    window.mode = WindowMode.windowed;
+                }
+            }
+        });
+
+        maximizeButton = getNodeT(Button, "vbox/menuBarControl/hbox/maximizeButton");
+        maximizeButton.addThemeStyleboxOverride("normal", styleBoxEmpty);
+        maximizeButton.focusMode = FocusModeEnum.none;
+        maximizeButton.addThemeFontOverride("font", buttonFont);
+        maximizeButton.text = "🗗";
+        maximizeButton.alignment = HorizontalAlignment.center;
+        if (window.mode != WindowMode.windowed) {
+            maximizeButton.text = "🗗";
+        }
+        else {
+            maximizeButton.text = "🗖";
+        }
+        maximizeButton.pressed.add(() -> {
+            if (window.mode != WindowMode.windowed) {
+                maximizeButton.text = "🗖";
+                var maximizedSize = window.size;
+                window.mode = WindowMode.windowed;
+                if (window.size.x == maximizedSize.x && window.size.y == maximizedSize.y) {
+                    window.size = ogWindowSize;
+                    window.moveToCenter();
+                }
+                else {
+                    window.size = windowSize;
+                }
+            }
+            else if (window.mode == WindowMode.windowed) {
+                maximizeButton.text = "🗗";
+                windowSize = window.size;
+                window.mode = WindowMode.maximized;
+            }
+        });
+
+        var closeButton = getNodeT(Button, "vbox/menuBarControl/hbox/closeButton");
+        closeButton.addThemeStyleboxOverride("normal", styleBoxEmpty);
+        closeButton.focusMode = FocusModeEnum.none;
+        closeButton.addThemeFontOverride("font", buttonFont);
+        closeButton.text = "🗙";
+        closeButton.alignment = HorizontalAlignment.center;
+        closeButton.pressed.add(() -> {
+            App.exit(0);
+        });
+
+        if (OSService.getName() == "macOS") {
+            iconContainer.hide();
+            minimizeButton.hide();
+            maximizeButton.hide();
+            closeButton.hide();
+        }
+
         window.filesDropped.connect(Callable.fromFunction(function(fileStringArray: TypedArray<String>) {
             var fileStringArr: Array<String> = fileStringArray;
             if (fileStringArr.length == 0) {
@@ -174,7 +383,60 @@ class PlayerView extends Widget {
             deviceTypeStr = "XR";
         }
         aboutString += "Device Type: " + deviceTypeStr + "\n";
-        Debug.info(aboutString, "About Sunaba Studio");
+        Debug.info(aboutString, "About Sunaba Player");
+    }
+
+    public override function onProcess(delta:Float) {
+        timeSinceClick -= delta;
+        if (timeSinceClick <= 0.0) {
+            timeSinceClick = 1.0;
+            if (clickcount != 0) {
+                clickcount = 0;
+            }
+        }
+
+        if (OSService.getName() != "macOS") {
+            window = getWindow();
+            if (window != null) {
+                if (window.mode != WindowMode.windowed) return;
+
+                var windowsize = window.getVisibleRect().size;
+
+                var mousePosition = window.getMousePosition();
+                if (mousePosition.x < resizeThreshold && mousePosition.y < resizeThreshold) { // Top left
+                    DisplayService.cursorSetShape(CursorShape.fdiagsize);
+                    return;
+                }
+                if (mousePosition.x > windowsize.x - resizeThreshold && mousePosition.y < resizeThreshold) { // Top Right
+                    DisplayService.cursorSetShape(CursorShape.bdiagsize);
+                    return;
+                }
+                if (mousePosition.x < resizeThreshold && mousePosition.y > windowsize.y - resizeThreshold) { // Bottom left
+                    DisplayService.cursorSetShape(CursorShape.bdiagsize);
+                    return;
+                }
+                if (mousePosition.x > windowsize.x - resizeThreshold && mousePosition.y > windowsize.y - resizeThreshold) { // Bottom Right
+                    DisplayService.cursorSetShape(CursorShape.fdiagsize);
+                    return;
+                }
+                if (mousePosition.x < resizeThreshold) { // left
+                    DisplayService.cursorSetShape(CursorShape.hsize);
+                    return;
+                }
+                if (mousePosition.x > windowsize.x - resizeThreshold) { // Right
+                    DisplayService.cursorSetShape(CursorShape.hsize);
+                    return;
+                }
+                if (mousePosition.y < resizeThreshold) { // Top
+                    DisplayService.cursorSetShape(CursorShape.vsize);
+                    return;
+                }
+                if (mousePosition.y > windowsize.y - resizeThreshold) { // Bottom
+                    DisplayService.cursorSetShape(CursorShape.vsize);
+                    return;
+                }
+            }
+        }
     }
 
     public override function onInput(event: InputEvent) {
@@ -197,6 +459,79 @@ class PlayerView extends Widget {
                 App.exit(0);
             }
         }
+
+        if (OSService.getName() != "macOS") {
+            if (event.native.isClass("InputEventMouseButton")) {
+                var eventMouseButton = Reference.castTo(event, InputEventMouseButton);
+                window = getWindow();
+                if (window.mode != WindowMode.windowed) return;
+                if (
+                    eventMouseButton.buttonIndex == MouseButton.left &&
+                    eventMouseButton.pressed
+                ) {
+                    var localX = eventMouseButton.position.x;
+                    var localY = eventMouseButton.position.y;
+
+                    // Top left
+                    if (localX < resizeThreshold && localY < resizeThreshold) {
+                        DisplayService.cursorSetShape(CursorShape.fdiagsize);
+                        window.startResize(WindowResizeEdge.topLeft);
+                        return;
+                    }
+                    // Top Right
+                    if (
+                        localX > window.getVisibleRect().size.x - resizeThreshold &&
+                        localY < resizeThreshold
+                    ) {
+                        DisplayService.cursorSetShape(CursorShape.bdiagsize);
+                        window.startResize(WindowResizeEdge.topRight);
+                        return;
+                    }
+                    // Bottom left
+                    if (
+                        localX < resizeThreshold &&
+                        localY > window.getVisibleRect().size.y - resizeThreshold
+                    ) {
+                        DisplayService.cursorSetShape(CursorShape.bdiagsize);
+                        window.startResize(WindowResizeEdge.bottomLeft);
+                        return;
+                    }
+                    // Bottom Right
+                    if (
+                        localX > window.getVisibleRect().size.x - resizeThreshold &&
+                        localY > window.getVisibleRect().size.y - resizeThreshold
+                    ) {
+                        DisplayService.cursorSetShape(CursorShape.fdiagsize);
+                        window.startResize(WindowResizeEdge.bottomRight);
+                        return;
+                    }
+                    // Left
+                    if (localX < resizeThreshold) {
+                        DisplayService.cursorSetShape(CursorShape.hsize);
+                        window.startResize(WindowResizeEdge.left);
+                        return;
+                    }
+                    // Right
+                    if (localX > window.getVisibleRect().size.x - resizeThreshold) {
+                        DisplayService.cursorSetShape(CursorShape.hsize);
+                        window.startResize(WindowResizeEdge.right);
+                        return;
+                    }
+                    // Top
+                    if (localY < resizeThreshold) {
+                        DisplayService.cursorSetShape(CursorShape.vsize);
+                        window.startResize(WindowResizeEdge.top);
+                        return;
+                    }
+                    // Bottom
+                    if (localY > window.getVisibleRect().size.y - resizeThreshold) {
+                        DisplayService.cursorSetShape(CursorShape.vsize);
+                        window.startResize(WindowResizeEdge.bottom);
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     inline  function toggleMenuBar() {
@@ -209,7 +544,18 @@ class PlayerView extends Widget {
             window.mode = WindowMode.fullscreen;
         }
         else {
-            window.mode = WindowMode.windowed;
+            if (isMaximized == true) {
+                window.mode = WindowMode.maximized;
+            }
+            else {
+                window.mode = WindowMode.windowed;
+            }
+        }
+        if (window.mode != WindowMode.windowed) {
+            maximizeButton.text = "🗗";
+        }
+        else {
+            maximizeButton.text = "🗖";
         }
     }
 
