51a031a853ef2cd3816911097a2502bd79026eea
Lucas C. Abbas <lucasbear9@gmail.com>
2026-02-14 20:33:19 -0900

    Updated Engine, Added Native Dialogs

--- Engine/DesktopApp.gd
+++ Engine/DesktopApp.gd
@@ -1,2 +1,45 @@
 extends App
 class_name DesktopApp
+
+
+func _errord(msg: String, title: String) -> void:
+	var dialog = NativeAcceptDialog.new()
+	dialog.title = title
+	dialog.dialog_text = msg
+	dialog.dialog_icon = NativeAcceptDialog.ICON_ERROR
+	add_child(dialog)
+	dialog.canceled.connect(func():
+		dialog.queue_free()
+	)
+	dialog.confirmed.connect(func():
+		dialog.queue_free()
+	)
+	dialog.show()
+
+func _warnd(msg: String, title: String) -> void:
+	var dialog = NativeAcceptDialog.new()
+	dialog.title = title
+	dialog.dialog_text = msg
+	dialog.dialog_icon = NativeAcceptDialog.ICON_WARNING
+	add_child(dialog)
+	dialog.canceled.connect(func():
+		dialog.queue_free()
+	)
+	dialog.confirmed.connect(func():
+		dialog.queue_free()
+	)
+	dialog.show()
+
+func _infod(msg: String, title: String) -> void:
+	var dialog = NativeAcceptDialog.new()
+	dialog.title = title
+	dialog.dialog_text = msg
+	dialog.dialog_icon = NativeAcceptDialog.ICON_INFO
+	add_child(dialog)
+	dialog.canceled.connect(func():
+		dialog.queue_free()
+	)
+	dialog.confirmed.connect(func():
+		dialog.queue_free()
+	)
+	dialog.show()
