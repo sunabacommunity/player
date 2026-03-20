93d2b82ce3a8437417437574b77f122d4e20fb16
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-20 11:02:46 -0800

    burmped version to 0.7.14

--- src/sunaba/player/PlayerView.hx
+++ src/sunaba/player/PlayerView.hx
@@ -390,7 +390,7 @@ class PlayerView extends Widget {
 
     function buildAboutDialog(dialog: AcceptDialog) {
         var aboutString = "Sunaba Player\n";
-        aboutString += "Version 0.7.13\n";
+        aboutString += "Version 0.7.14\n";
         aboutString += "(C) 2022-2025 mintkat\n";
         aboutString += "\n";
 
